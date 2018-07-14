296d87bafa58b2ed9c4db17d1903022d1fab4932
Raul <jrtapia@asu.edu>
2018-07-13 19:15:44 -0700

    Day 1 pt. 2

--- invitations.py
+++ invitations.py
@@ -7,13 +7,3 @@ with open('guests.csv', 'w', newline='') as csvfile:
     writer.writeheader()
     writer.writerow({'First Name': 'Raul', 'Last Name': 'Tapia-Martinez', 'Response': 'Yes', 'Guests': '3'})
     writer.writerow({'First Name': 'Abbie', 'Last Name': 'Prescott', 'Response': 'Yes', 'Guests': '3'})
-
-# with open('eggs.csv', 'w', newline='') as csvfile:
-#     writer = csv.writer(csvfile, delimiter=' ',
-#                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#     writer.writerow(['Spam'] * 5 + ['Baked Beans'])
-#     writer.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
-
-# with open('some.csv', 'w', newline='') as csvfile:
-#     writer = csv.writer(csvfile)
-#     writer.writerow('Hello World!')
