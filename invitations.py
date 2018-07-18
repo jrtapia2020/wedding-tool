a6220dbcfbdfc9c6aaf90fc3e0cea85b09b0bfdc
Raul <jrtapia@asu.edu>
2018-07-18 15:16:19 -0700

    Day 3 pt 1.

--- invitations.py
+++ invitations.py
@@ -3,28 +3,24 @@
 import argparse
 import csv
 
-# TODO: Figure out what the point of the command line argument is and how to use it properly.
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--verbose", help="increase output verbosity",
-#                     action="store_true")
-# args = parser.parse_args()
-# if args.verbose:
-#     print("verbosity turned on")
 
+# TODO: Figure out what the point of the command line argument is and how to use it properly.
 # TODO: Find a way to make sure it creates new lines in the CSV file each time the prompt is answered.
 
-print("\n\nGuest Information\n")
-first_name = input('First Name: ')
-last_name = input('Last Name: ')
-response = input('Will you be attending? Yes/No: ')
-if response == 'Yes' or 'yes':
-    guests = int(input('How many guests (including yourself) will be attending? '))
-    print('Information Saved.')
-else:
-    print('Information Saved.')
-
-
 def main():
+    parser = argparse.ArgumentParser(prog='Invitation Responses')
+    parser.add_argument('input')
+    args = parser.parse_args()
+
+    # TODO: Where does args.input go? How do I save it to the CSV file?
+
+    print("\n\nGuest Information\n")
+    first_name = input('First Name: ')
+    last_name = input('Last Name: ')
+    response = input('Will they be attending? Yes/No: ')
+    guests = int(input('How many guests will be attending? '))
+    print('Information Saved.')
+
     with open('guests.csv', 'w', newline='') as csvfile:
         fieldnames = ['First Name', 'Last Name', 'Response', 'Guests']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
