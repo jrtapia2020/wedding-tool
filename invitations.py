efa9e0f963f7982da918ccd2b6ffc8f69c49e071
Raul <jrtapia@asu.edu>
2018-07-20 12:55:15 -0700

    Day 4.

--- invitations.py
+++ invitations.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python
 
 import argparse
+import os.path
 import csv
 
 
-# TODO: Figure out what the point of the command line argument is and how to use it properly.
-# TODO: Find a way to make sure it creates new lines in the CSV file each time the prompt is answered.
+# TODO: Find a way to make sure it get rid of the extra line of white space isn't added.
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(prog='Invitation Responses')
-    parser.add_argument('input')
+    parser.add_argument('first_name', help='First name of guest.')
+    parser.add_argument('last_name', help='Last name of guest.')
+    parser.add_argument('response', help='Guest response (Yes/No).')
+    parser.add_argument('guest_count', help='Number of guests attending (including guest responding).')
     args = parser.parse_args()
 
-    # TODO: Where does args.input go? How do I save it to the CSV file?
+    file_exists = os.path.isfile('guests.csv')
 
-    print("\n\nGuest Information\n")
-    first_name = input('First Name: ')
-    last_name = input('Last Name: ')
-    response = input('Will they be attending? Yes/No: ')
-    guests = int(input('How many guests will be attending? '))
+    with open('guests.csv', 'a') as csvfile:
+        headers = ['First Name', 'Last Name', 'Response', 'Guest Count']
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+
+        if not file_exists:
+            writer.writeheader()  # If the file doesn't exist yet, write a header.
+
+        writer.writerow(
+            {'First Name': args.first_name,
+             'Last Name': args.last_name,
+             'Response': args.response,
+             'Guest Count': args.guest_count})
+
     print('Information Saved.')
-
-    with open('guests.csv', 'w', newline='') as csvfile:
-        fieldnames = ['First Name', 'Last Name', 'Response', 'Guests']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        writer.writerow({'First Name': first_name, 'Last Name': last_name, 'Response': response, 'Guests': guests})
-
-    print(args.input)
 
 
 if __name__ == '__main__':
